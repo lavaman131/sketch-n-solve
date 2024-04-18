ffc43a633a2750d7dd6887faef25d083b026361e
lavaman131 <lavaalex3@gmail.com>
2024-04-18 12:18:57 -0400

    added demo

--- tools/demo.py
+++ tools/demo.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import numpy.linalg as LA
 from sketch_n_solve.solve.least_squares import LeastSquares
 
 sketch_fn = "sparse_sign"
@@ -12,6 +12,9 @@ lsq = LeastSquares(sketch_fn, seed)
 print(lsq.sketch_and_apply.__doc__)
 
 x_hat, time_elapsed, x_hats = lsq.sketch_and_apply(A, b, sparsity_parameter=None)
+
+print(x, x_hat)
+print("residual", LA.norm(A @ x_hat - A @ x))
 
 is_close = np.allclose(x_hat, x)
 print(f"x_hat is close to x => {is_close}")
